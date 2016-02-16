9c24d4bb0f9170928afde7c7d8c012891fe7af5e
Bob W. Hogg <hogg.bob.2@gmail.com>
2016-02-15 21:31:38 -0800

    much nicer docs

--- GulpFlow.js
+++ GulpFlow.js
@@ -15,20 +15,35 @@ var flow = require("flow-bin");
 
 const PLUGIN_NAME = "gulp-flow";
 
-/**
- * GulpFlow is a Gulp plugin for type-checking code with `flow`.
- *
- * @class GulpFlow
- */
 module.exports = Class.extend(
 {
+    /**
+     * GulpFlow is a Gulp plugin for type-checking code with `flow`.
+     *
+     * @class GulpFlow
+     * @constructor
+     */
     init: function()
     {
+        /**
+         * Options to pass to the `flow` binary.
+         *
+         * @property options
+         * @type string[]
+         * @private
+         */
         this.options = [
             "--json"
         ];
     },
 
+    /**
+     * Check for type errors and store them in `this.results`.
+     * Only compatible with buffers at the moment.
+     *
+     * @method check
+     * @return {Object} A gulp-compatible stream.
+     */
     check: function()
     {
         var me = this;
@@ -55,6 +70,14 @@ module.exports = Class.extend(
                 // flow normally exits with a non-zero status if errors are found
                 output = e.stdout.toString("utf-8");
             }
+            
+            /**
+             * Results of the type check.
+             *
+             * @property results
+             * @type String
+             * @private
+             */
             me.results[file.path] = output;
 
             this.push(file);
@@ -62,12 +85,19 @@ module.exports = Class.extend(
         });
     },
 
+    /**
+     * Dump the results of the type check to the command line.
+     *
+     * @method reporter
+     * @return {Object} A gulp-compatible stream.
+     */
     reporter: function()
     {
         var me = this;
         return through.obj(function(file, encoding, callback)
         {
             _.forEach(me.results, gutil.log);
+            // FIXME: I forgot to pass the files along!
         });
     }
 });
