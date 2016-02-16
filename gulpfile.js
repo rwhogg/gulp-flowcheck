e70df735085c2f305b6635e187e1ee4380bbb89f
Bob W. Hogg <hogg.bob.2@gmail.com>
2016-02-15 20:51:36 -0800

    reorganize scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var ava = require("gulp-ava");
 var jscs = require("gulp-jscs");
+var yuidoc = require("gulp-yuidoc");
 
 gulp.task("lint", function()
 {
@@ -13,4 +14,12 @@ gulp.task("test", function()
 {
     return gulp.src("test.js")
         .pipe(ava());
+});
+
+gulp.task("doc", function()
+{
+    return gulp.src("GulpFlow.js")
+        .pipe(yuidoc.parser())
+        .pipe(yuidoc.generator())
+        .pipe(gulp.dest("docs"));
 });
