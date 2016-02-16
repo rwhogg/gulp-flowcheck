f962a916eaffbe1e13641bec67c08cf8489571f4
Bob W. Hogg <hogg.bob.2@gmail.com>
2016-02-15 17:08:49 -0800

    lots of code. who knows if it works?

--- GulpFlow.js
+++ GulpFlow.js
@@ -1,1 +1,46 @@
-var x = 5;
+var Class = require("yajscf");
+var through = require("through2");
+var gutil = require("gulp-util");
+var PluginError = gutil.PluginError;
+var execFile = require("child_process").execFileSync;
+var flow = require("flow-bin");
+
+const PLUGIN_NAME = "gulp-flow";
+
+module.exports = Class.extend(
+{
+    init: function()
+    {
+        this.options =
+        [
+            "--old-output-format",
+            "--one-line"
+        ];
+    },
+
+    doc: function()
+    {
+        var me = this;
+        return through.obj(function(file, encoding, callback)
+        {
+            if(file.isStream())
+            {
+                throw new PluginError("streams are not supported (yet?)");
+            }
+            try
+            {
+                var output = execFileSync(flow, me.options, {
+                    input: file.contents
+                });
+            }
+            catch(e)
+            {
+                throw new PluginError(e.toString());
+            }
+            gutil.log(output);
+
+            this.push(file);
+            callback();
+        });
+    }
+});
