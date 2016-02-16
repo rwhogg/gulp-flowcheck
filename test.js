935f5e3c52b7b217241aa8f9af735499a5ffe0d2
Bob W. Hogg <hogg.bob.2@gmail.com>
2016-02-15 18:24:52 -0800

    depend on fork of gulp-ava until a bug is fixed upstream

--- test.js
+++ test.js
@@ -1,22 +1,24 @@
 import test from "ava";
 
-var es = require("event-stream");
 var fs = require("fs");
 var File = require("vinyl");
 var GulpFlow = require("./GulpFlow.js");
 
 test.serial("buffer mode", function(t)
 {
-    var contents = es.readArray(fs.readFileSync("test/buffer.js", "utf-8").split(/\w/));
+    var contents = new Buffer(fs.readFileSync("test/buffer.js", "utf-8").split(/\w/));
+    console.log(contents);
     var fakeFile = new File({
         contents: contents
     });
     var gulpFlow = new GulpFlow();
     var checker = gulpFlow.check();
-    /*checker.write(fakeFile);
-    checker.once("data", function(file)
+    //console.log(checker.write);
+    checker.write(fakeFile);
+    /*checker.once("data", function(file)
     {
         t["true"](file.isBuffer());
         t.is(file.contents, contents);
+        done();
     });*/
 });
