1d3cccf3c15eaae1e50d7819380e58b2f4815cc4
Bob W. Hogg <hogg.bob.2@gmail.com>
2016-02-15 19:42:35 -0800

    confirm that we failed

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import test from "ava";
 
+var _ = require("underscore");
 var fs = require("fs");
 var File = require("vinyl");
 var GulpFlow = require("./GulpFlow.js");
@@ -9,17 +10,30 @@ test.serial("buffer mode", function(t)
     var stringContents = fs.readFileSync("test/buffer.js", "utf-8");
     var contents = new Buffer(stringContents);
 
+    var fakeFilePath = "test/buffer.js";
     var fakeFile = new File({
         contents: contents,
-        path: "test/buffer.js"
+        path: fakeFilePath
     });
     var gulpFlow = new GulpFlow();
     var checker = gulpFlow.check();
     checker.write(fakeFile);
     checker.once("data", function(file)
     {
+        // check that the file itself did not change
         t["true"](file.isBuffer());
         t.is(file.contents.toString("utf-8"), stringContents);
+        
+        // check that the results are correct
         console.dir(gulpFlow.results);
+        _.forEach(gulpFlow.results, function(resultJSON, index)
+        {
+            // check that the path is correct
+            t.is(index, fakeFilePath);
+            
+            // check that we failed
+            var result = JSON.parse(resultJSON);
+            t["false"](result.passed);
+        });
     });
 });
