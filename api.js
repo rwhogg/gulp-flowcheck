bbd07700bab308b1a3cd467c8fe1a5e34c01f9c8
Bob W. Hogg <hogg.bob.2@gmail.com>
2016-02-17 08:40:13 -0800

    Initial commit of gh-pages

---
+++ api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "GulpFlow"
+    ],
+    "modules": [
+        "GulpFlow"
+    ],
+    "allModules": [
+        {
+            "displayName": "GulpFlow",
+            "name": "GulpFlow",
+            "description": "GulpFlow provides a [gulp](https://gulpjs.com) plugin for [Flow](http://flowtype.org)."
+        }
+    ],
+    "elements": []
+} };
+});
